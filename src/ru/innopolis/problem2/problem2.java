5a89092340034ece38c7d2f5010c0b09a1166475
SokolOFFF <egor1sokolov@gmail.com>
2022-04-11 13:40:55 +0300

    pr 2

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,16 @@
 // your solution here
 //fix all the errors including package errors
+interface ICommand
+{
+    void Execute();
+}
+
+class Sell : ICommand{
+    public void Execute(){
+    }
+}
+
+class  Buy : ICommand{
+    public void Execute(){
+    }
+}
